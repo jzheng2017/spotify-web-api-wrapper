855c48519da1484c2da7c0fe58d23348d596e24a
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-13 00:09:34 +0100

    add(config): api url constants

---
+++ src/main/java/spotify/config/ApiUrl.java
@@ -0,0 +1,9 @@
+package spotify.config;
+
+public class ApiUrl {
+    public static final String API_BASE_URL = "api.spotify.com";
+    public static final String ACCOUNTS_BASE_URL = "accounts.spotify.com";
+    public static final String API_URL_HTTPS = "https://api.spotify.com";
+    public static final String ACCOUNTS_URL_HTTPS = "https://accounts.spotify.com";
+    public static final String VERSION = "v1";
+}
