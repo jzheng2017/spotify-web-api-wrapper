3acfab730cdf4ecbdc6ccce3498875b023253e25
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-13 01:21:39 +0100

    change(config): dashes to api version

--- src/main/java/spotify/config/ApiUrl.java
+++ src/main/java/spotify/config/ApiUrl.java
@@ -5,5 +5,5 @@ public class ApiUrl {
     public static final String ACCOUNTS_BASE_URL = "accounts.spotify.com";
     public static final String API_URL_HTTPS = "https://api.spotify.com";
     public static final String ACCOUNTS_URL_HTTPS = "https://accounts.spotify.com";
-    public static final String VERSION = "v1";
+    public static final String VERSION = "/v1/";
 }
