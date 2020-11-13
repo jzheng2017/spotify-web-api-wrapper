9aebca5e29c37e4bb483c6edc2b85116ffab5d62
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-13 16:18:11 +0100

    add(retrofit): get tracks http function

--- src/main/java/spotify/retrofit/services/TrackService.java
+++ src/main/java/spotify/retrofit/services/TrackService.java
@@ -6,9 +6,13 @@ import retrofit2.http.Header;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
 import spotify.models.TrackFull;
+import spotify.models.TrackFullList;
 
 public interface TrackService {
 
     @GET("tracks/{id}")
     Call<TrackFull> getTrack(@Header("Authorization") String accessToken, @Path("id") String trackId, @Query("market") String market);
+
+    @GET("tracks")
+    Call<TrackFullList> getTracks(@Header("Authorization") String accessToken, @Query("ids") String trackIds, @Query("market") String market);
 }
