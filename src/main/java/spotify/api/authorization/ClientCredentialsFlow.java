5cbe8e9428987c12b67c67d742f7adbc32f0ee6a
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-14 17:43:39 +0100

    docs(api): add javadoc to ClientCredentialsFlow class

--- src/main/java/spotify/api/authorization/ClientCredentialsFlow.java
+++ src/main/java/spotify/api/authorization/ClientCredentialsFlow.java
@@ -14,6 +14,18 @@ import spotify.retrofit.services.ClientCredentialsFlowService;
 
 import java.io.IOException;
 
+/**
+ *
+ * This class is used to get an access token through the Client Credentials Flow.
+ * Executing this step will result in an access token.
+ * This access token can only be used to access endpoints with publicly available information,
+ * so therefore information about the user can not be accessed through this access token.
+ *
+ * Authorization Code Flow is recommended to access private information, such as user information.
+ * @see AuthorizationCodeFlow
+ *
+ * @author Jiankai Zheng
+ */
 public class ClientCredentialsFlow {
     private final Retrofit httpClient;
     private ClientCredentialsFlowTokenResponse clientCredentialsFlowTokenResponse;
@@ -22,6 +34,15 @@ public class ClientCredentialsFlow {
         httpClient = RetrofitClientFactory.getRetrofitClient(ApiUrl.ACCOUNTS_URL_HTTPS);
     }
 
+    /**
+     *
+     * Handles the Client Credentials Flow. This results in an access token.
+     *
+     * See the Spotify Authorization Guide for information.
+     *
+     * @param clientId id of the client
+     * @param clientSecret secret of the client
+     */
     public void doClientCredentialsFlow(String clientId, String clientSecret) {
         ClientCredentialsFlowService clientCredentialsFlowService = httpClient.create(ClientCredentialsFlowService.class);
         final String base64EncodedBasicAuth = Credentials.basic(clientId, clientSecret);
@@ -41,6 +62,12 @@ public class ClientCredentialsFlow {
         }
     }
 
+    /**
+     *
+     * Gets an object containing values related to the access token.
+     *
+     * @return an object containing values like the access token
+     */
     public ClientCredentialsFlowTokenResponse getClientCredentialsFlowToken() {
         return clientCredentialsFlowTokenResponse;
     }
