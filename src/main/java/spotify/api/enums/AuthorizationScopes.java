ffa91bf6a9a270f9ed401aa9b662c67b8fd1a301
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-14 16:46:12 +0100

    add(api): enums for spotify authorization scopes

---
+++ src/main/java/spotify/api/enums/AuthorizationScopes.java
@@ -0,0 +1,34 @@
+package spotify.api.enums;
+
+public enum AuthorizationScopes {
+    UGC_IMAGE_UPLOAD("ugc-image-upload"),
+    USER_READ_RECENTLY_PLAYED("user-read-recently-played"),
+    USER_TOP_READ("user-top-read"),
+    USER_READ_PLAYBACK_POSITION("user-read-playback-position"),
+    USER_READ_PLAYBACK_STATE("user-read-playback-state"),
+    USER_MODIFY_PLAYBACK_STATE("user-modify-playback-state"),
+    USER_READ_CURRENTLY_PLAYING("user-read-currently-playing"),
+    APP_REMOTE_CONTROL("app-remote-control"),
+    STREAMING("streaming"),
+    PLAYLIST_MODIFY_PUBLIC("playlist-modify-public"),
+    PLAYLIST_MODIFY_PRIVATE("playlist-modify-private"),
+    PLAYLIST_READ_PRIVATE("playlist-read-private"),
+    PLAYLIST_READ_COLLABORATIVE("playlist-read-collaborative"),
+    USER_FOLLOW_MODIFY("user-follow-modify"),
+    USER_FOLLOW_READ("user-follow-read"),
+    USER_LIBRARY_MODIFY("user-library-modify"),
+    USER_LIBRARY_READ("user-library-read"),
+    USER_READ_EMAIL("user-read-email"),
+    USER_READ_PRIVATE("user-read-private");
+
+    private final String value;
+
+    AuthorizationScopes(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return this.value;
+    }
+}
