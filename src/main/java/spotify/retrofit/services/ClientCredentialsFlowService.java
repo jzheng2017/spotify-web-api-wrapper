ee017bbf187950ca58ce994384bc3af5ec611474
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-14 17:23:04 +0100

    change(retrofit)!: grant type changed from String value to enum class GrantType

--- src/main/java/spotify/retrofit/services/ClientCredentialsFlowService.java
+++ src/main/java/spotify/retrofit/services/ClientCredentialsFlowService.java
@@ -5,11 +5,12 @@ import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import spotify.api.enums.GrantType;
 import spotify.models.authorization.ClientCredentialsFlowTokenResponse;
 
 public interface ClientCredentialsFlowService {
 
     @POST("api/token")
     @FormUrlEncoded
-    Call<ClientCredentialsFlowTokenResponse> getToken(@Header("Authorization") String basicAuth, @Field("grant_type") String grantType);
+    Call<ClientCredentialsFlowTokenResponse> getToken(@Header("Authorization") String basicAuth, @Field("grant_type") GrantType grantType);
 }
