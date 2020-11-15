1ca9756a4c19e09df773a9d18abc85f87ecfc3bf
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-15 02:12:01 +0100

    add(models): authorization code flow token model

---
+++ src/main/java/spotify/models/authorization/AuthorizationCodeFlowTokenResponse.java
@@ -0,0 +1,49 @@
+package spotify.models.authorization;
+
+public class AuthorizationCodeFlowTokenResponse {
+    private String accessToken;
+    private String tokenType;
+    private String scope;
+    private String refreshToken;
+    private int expiresIn;
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public String getTokenType() {
+        return tokenType;
+    }
+
+    public void setTokenType(String tokenType) {
+        this.tokenType = tokenType;
+    }
+
+    public String getScope() {
+        return scope;
+    }
+
+    public void setScope(String scope) {
+        this.scope = scope;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    public int getExpiresIn() {
+        return expiresIn;
+    }
+
+    public void setExpiresIn(int expiresIn) {
+        this.expiresIn = expiresIn;
+    }
+}
