74eec3060178917e17890a0dde902e4bc18a6822
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-15 02:13:16 +0100

    add(retrofit): authorization code flow http class

---
+++ src/main/java/spotify/retrofit/services/AuthorizationCodeFlowService.java
@@ -0,0 +1,19 @@
+package spotify.retrofit.services;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Header;
+import retrofit2.http.POST;
+import spotify.api.enums.GrantType;
+import spotify.models.authorization.AuthorizationCodeFlowTokenResponse;
+
+public interface AuthorizationCodeFlowService {
+
+    @POST("api/token")
+    @FormUrlEncoded
+    Call<AuthorizationCodeFlowTokenResponse> getAccessAndRefreshToken(@Header("Authorization") String basicAuth,
+                                                                      @Field("code") String code,
+                                                                      @Field("redirect_uri") String uri,
+                                                                      @Field("grant_type") GrantType grantType);
+}
