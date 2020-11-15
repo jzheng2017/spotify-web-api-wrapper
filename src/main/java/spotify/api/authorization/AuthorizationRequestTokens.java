bc51761903d3fedf8ba9877445531193179521c8
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-15 02:19:14 +0100

    docs(api): add javadoc to AuthorizationRequestTokens

--- src/main/java/spotify/api/authorization/AuthorizationRequestTokens.java
+++ src/main/java/spotify/api/authorization/AuthorizationRequestTokens.java
@@ -14,6 +14,14 @@ import spotify.retrofit.services.AuthorizationCodeFlowService;
 
 import java.io.IOException;
 
+/**
+ * This class takes care of the second step in the Authorization Code Flow.
+ * Executing this step should result in an access and refresh token.
+ * <p>
+ * For more information see: {@link <a href="https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow">Authorization Code Flow</a>}
+ *
+ * @author Jiankai Zheng
+ */
 public class AuthorizationRequestTokens {
     private final Retrofit httpClient;
 
@@ -21,6 +29,14 @@ public class AuthorizationRequestTokens {
         this.httpClient = RetrofitClientFactory.getRetrofitClient(ApiUrl.ACCOUNTS_URL_HTTPS);
     }
 
+    /**
+     *
+     * @param clientId the id of the client
+     * @param clientSecret the secret of the client
+     * @param code the authorization code returned from the initial request to the Account /authorize endpoint
+     * @param redirectUri the redirect uri supplied when requesting the authorization code
+     * @return an object containing values like the access and refresh token
+     */
     public AuthorizationCodeFlowTokenResponse getAccessAndRefreshToken(String clientId, String clientSecret, String code, String redirectUri) {
         final AuthorizationCodeFlowService authorizationCodeFlowService = httpClient.create(AuthorizationCodeFlowService.class);
         final String base64EncodedBasicAuth = Credentials.basic(clientId, clientSecret);
