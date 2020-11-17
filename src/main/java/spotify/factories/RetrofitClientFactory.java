5d09bf29a038a1f4149d80cf4062420b38f0bb7f
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-17 23:49:02 +0100

    add(factories): logging to factory

--- src/main/java/spotify/factories/RetrofitClientFactory.java
+++ src/main/java/spotify/factories/RetrofitClientFactory.java
@@ -1,11 +1,15 @@
 package spotify.factories;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClientFactory {
+    private final static Logger logger = LoggerFactory.getLogger(RetrofitClientFactory.class);
 
     public static Retrofit getRetrofitClient(String baseUrl) {
+        logger.trace(String.format("Building Retrofit HTTP client with base url %s.", baseUrl));
         return new Retrofit.Builder()
                 .baseUrl(baseUrl)
                 .addConverterFactory(GsonConverterFactory
