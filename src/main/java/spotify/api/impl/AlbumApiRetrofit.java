bcc3cbd723eced389b5fa4e2ba3c24fed1816380
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-17 23:49:36 +0100

    add(api): logging to get album flow

--- src/main/java/spotify/api/impl/AlbumApiRetrofit.java
+++ src/main/java/spotify/api/impl/AlbumApiRetrofit.java
@@ -1,5 +1,7 @@
 package spotify.api.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
@@ -14,6 +16,7 @@ import spotify.retrofit.services.AlbumService;
 import java.io.IOException;
 
 public class AlbumApiRetrofit implements AlbumApi {
+    private final Logger logger = LoggerFactory.getLogger(AlbumApiRetrofit.class);
     private AlbumService albumService;
     private final String accessToken;
 
@@ -24,19 +27,25 @@ public class AlbumApiRetrofit implements AlbumApi {
 
     @Override
     public AlbumFull getAlbum(String albumId) {
+        logger.trace("Constructing HTTP call to fetch an album.");
         Call<AlbumFull> httpCall = albumService.getAlbum("Bearer " + this.accessToken, albumId);
 
         try {
+            logger.info("Executing HTTP call to fetch an album.");
             Response<AlbumFull> response = httpCall.execute();
 
             ResponseChecker.throwIfRequestHasNotBeenFulfilledCorrectly(response.errorBody());
 
+            logger.info("Album has been successfully fetched.");
             return response.body();
         } catch (IOException e) {
+            logger.error("Fetching album has failed.");
             throw new HttpRequestFailedException(e.getMessage());
         }
     }
+
     private void setup() {
+        logger.trace("Requesting Retrofit HTTP client.");
         Retrofit httpClient = RetrofitClientFactory.getRetrofitClient(ApiUrl.API_URL_HTTPS + ApiUrl.VERSION);
 
         albumService = httpClient.create(AlbumService.class);
