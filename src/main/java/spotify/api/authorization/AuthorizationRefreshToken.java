87dedffc0c4004c6d4e2d85422467b8bae10b810
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-18 17:51:06 +0100

    add(api): logging to authorization refresh token

--- src/main/java/spotify/api/authorization/AuthorizationRefreshToken.java
+++ src/main/java/spotify/api/authorization/AuthorizationRefreshToken.java
@@ -1,6 +1,8 @@
 package spotify.api.authorization;
 
 import okhttp3.Credentials;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
@@ -15,17 +17,21 @@ import spotify.retrofit.services.AuthorizationCodeFlowService;
 import java.io.IOException;
 
 public class AuthorizationRefreshToken {
-    private Retrofit httpClient;
+    private final Logger logger = LoggerFactory.getLogger(AuthorizationRefreshToken.class);
+    private final Retrofit httpClient;
 
     public AuthorizationRefreshToken() {
+        logger.trace("Requesting Retrofit HTTP client.");
         httpClient = RetrofitClientFactory.getRetrofitClient(ApiUrl.ACCOUNTS_URL_HTTPS);
     }
 
     public AuthorizationCodeFlowTokenResponse refreshAccessToken(String clientId, String clientSecret, String refreshToken) {
         final AuthorizationCodeFlowService authorizationCodeFlowService = httpClient.create(AuthorizationCodeFlowService.class);
 
+        logger.trace("Encoding client id and secret to base 64.");
         final String base64EncodedBasicAuth = Credentials.basic(clientId, clientSecret);
 
+        logger.trace("Constructing HTTP call to refresh the access token.");
         final Call<AuthorizationCodeFlowTokenResponse> httpCall = authorizationCodeFlowService
                 .refreshAccessToken(
                         base64EncodedBasicAuth,
@@ -33,14 +39,18 @@ public class AuthorizationRefreshToken {
                         GrantType.REFRESH_TOKEN);
 
         try {
+            logger.info("Executing HTTP call to refresh the access token.");
             final Response<AuthorizationCodeFlowTokenResponse> response = httpCall.execute();
 
             if (response.body() == null) {
+                logger.error("Spotify has returned empty response body. This may mean the given credentials are invalid.");
                 throw new SpotifyAuthorizationFailedException("Refreshing the access token with the given credentials has failed!");
             }
 
+            logger.info("Access token has been successfully refreshed.");
             return response.body();
         } catch (IOException e) {
+            logger.error("HTTP request to refresh the access token has failed.");
             throw new HttpRequestFailedException(e.getMessage());
         }
     }
