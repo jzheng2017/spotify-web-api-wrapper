20a8bb1522dcb0252f706149f787d66879f176c0
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-19 00:15:20 +0100

    add(models): list of full albums model

---
+++ src/main/java/spotify/models/albums/AlbumFullList.java
@@ -0,0 +1,15 @@
+package spotify.models.albums;
+
+import java.util.List;
+
+public class AlbumFullList {
+    private List<AlbumFull> albums;
+
+    public List<AlbumFull> getAlbums() {
+        return albums;
+    }
+
+    public void setAlbums(List<AlbumFull> albums) {
+        this.albums = albums;
+    }
+}
