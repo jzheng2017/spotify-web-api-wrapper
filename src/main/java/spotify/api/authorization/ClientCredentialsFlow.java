50c4c0202485f4ab2968eaca0891a8d02055eeb8
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-18 17:30:22 +0100

    add(api): logging to client credentials flow

--- src/main/java/spotify/api/authorization/ClientCredentialsFlow.java
+++ src/main/java/spotify/api/authorization/ClientCredentialsFlow.java
@@ -1,6 +1,8 @@
 package spotify.api.authorization;
 
 import okhttp3.Credentials;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import retrofit2.Call;
 import retrofit2.Response;
 import retrofit2.Retrofit;
@@ -26,9 +28,11 @@ import java.io.IOException;
  * @see AuthorizationCodeFlow
  */
 public class ClientCredentialsFlow {
+    private final Logger logger = LoggerFactory.getLogger(ClientCredentialsFlow.class);
     private final Retrofit httpClient;
 
     public ClientCredentialsFlow() {
+        logger.trace("Requesting Retrofit HTTP client.");
         httpClient = RetrofitClientFactory.getRetrofitClient(ApiUrl.ACCOUNTS_URL_HTTPS);
     }
 
@@ -43,19 +47,26 @@ public class ClientCredentialsFlow {
      */
     public ClientCredentialsFlowTokenResponse getAccessToken(String clientId, String clientSecret) {
         final ClientCredentialsFlowService clientCredentialsFlowService = httpClient.create(ClientCredentialsFlowService.class);
+
+        logger.trace("Encoding client id and secret to base 64.");
         final String base64EncodedBasicAuth = Credentials.basic(clientId, clientSecret);
 
+        logger.trace("Constructing HTTP call to fetch an access token.");
         final Call<ClientCredentialsFlowTokenResponse> httpCall = clientCredentialsFlowService.getToken(base64EncodedBasicAuth, GrantType.CLIENT_CREDENTIALS);
 
         try {
+            logger.info("Executing HTTP call to fetch an access token.");
             final Response<ClientCredentialsFlowTokenResponse> response = httpCall.execute();
 
             if (response.body() == null) {
+                logger.error("Spotify has returned empty response body. This may mean the given credentials are invalid.");
                 throw new SpotifyAuthorizationFailedException("Authorizing with the given credentials has failed!");
             }
 
+            logger.info("Access token has been successfully fetched.");
             return response.body();
         } catch (IOException e) {
+            logger.error("HTTP request to fetch an access token has failed.");
             throw new HttpRequestFailedException(e.getMessage());
         }
     }
