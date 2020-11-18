14b765e8a9ab72e9597ea0f987b55d1875a6a4fa
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-18 17:19:23 +0100

    add(factories): logging to gson factory

--- src/main/java/spotify/factories/GsonFactory.java
+++ src/main/java/spotify/factories/GsonFactory.java
@@ -3,13 +3,17 @@ package spotify.factories;
 import com.google.gson.FieldNamingPolicy;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 class GsonFactory {
+    private final static Logger logger = LoggerFactory.getLogger(GsonFactory.class);
 
     public static Gson getGson(FieldNamingPolicy fieldNamingPolicy) {
         GsonBuilder gsonBuilder = new GsonBuilder();
         gsonBuilder.setFieldNamingPolicy(fieldNamingPolicy);
 
+        logger.trace(String.format("Constructing Gson object with the FieldNamingPolicy %s", fieldNamingPolicy));
         return gsonBuilder.create();
     }
 
