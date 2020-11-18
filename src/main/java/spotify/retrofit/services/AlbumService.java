46ef42a49fe60d78e5363f450104289a0b89f5bd
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-19 00:15:43 +0100

    add(retrofit): http call for fetching multiple albums

--- src/main/java/spotify/retrofit/services/AlbumService.java
+++ src/main/java/spotify/retrofit/services/AlbumService.java
@@ -4,10 +4,15 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 import spotify.models.albums.AlbumFull;
+import spotify.models.albums.AlbumFullList;
 
 public interface AlbumService {
 
     @GET("albums/{id}")
     Call<AlbumFull> getAlbum(@Header("Authorization") String accessToken, @Path("id") String albumId);
+
+    @GET("albums")
+    Call<AlbumFullList> getAlbums(@Header("Authorization") String accessToken, @Query("ids") String albumIds, @Query("market") String market);
 }
