9e1a1a62d201644e96320d0059ba3d00180111a1
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-20 23:20:41 +0100

    add(retrofit): http call for fetching an artisst

---
+++ src/main/java/spotify/retrofit/services/ArtistService.java
@@ -0,0 +1,12 @@
+package spotify.retrofit.services;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Header;
+import retrofit2.http.Path;
+import spotify.models.artists.ArtistFull;
+
+public interface ArtistService {
+    @GET("artists/{id}")
+    Call<ArtistFull> getArtist(@Header("Authorization") String accesToken, @Path("id") String artistId);
+}
