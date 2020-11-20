5cc48089ad1e834e14ebeeb49f7c90fbeeb082d4
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-21 00:06:09 +0100

    add(models): collection of full artists

---
+++ src/main/java/spotify/models/artists/ArtistFullCollection.java
@@ -0,0 +1,15 @@
+package spotify.models.artists;
+
+import java.util.List;
+
+public class ArtistFullCollection {
+    private List<ArtistFull> artists;
+
+    public List<ArtistFull> getArtists() {
+        return artists;
+    }
+
+    public void setArtists(List<ArtistFull> artists) {
+        this.artists = artists;
+    }
+}
