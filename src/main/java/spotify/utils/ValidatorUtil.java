e57a195a3d7a4f5aa735b58a5f1ae7d8a3ad847d
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-20 22:38:07 +0100

    docs(validator): add javadoc to validator class

--- src/main/java/spotify/utils/ValidatorUtil.java
+++ src/main/java/spotify/utils/ValidatorUtil.java
@@ -3,9 +3,18 @@ package spotify.utils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Utility class that validates the values.
+ */
 public class ValidatorUtil {
     private final static Logger logger = LoggerFactory.getLogger(ValidatorUtil.class);
 
+    /**
+     * Checks if the passed in market is empty, if it is it will set it to null.
+     *
+     * @param market the market value
+     * @return market value
+     */
     public static String marketEmptyCheck(String market) {
         // this is done because retrofit ignores null values
         // when an empty market value is passed to spotify it will give an error saying the market does not exist
@@ -17,6 +26,12 @@ public class ValidatorUtil {
         return market;
     }
 
+    /**
+     * Validate filters.
+     *
+     * @param limit  the number of items to be returned
+     * @param offset the offset, 0 means starting from the first item
+     */
     public static void validateFilters(int limit, int offset) {
         logger.trace("Validating passed in values");
         logger.debug(String.format("Passed in values: limit = %d, offset = %d", limit, offset));
