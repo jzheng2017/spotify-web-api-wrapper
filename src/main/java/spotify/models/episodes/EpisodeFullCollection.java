c5391848d3ccbdf1fcd03ec7e6006767c7cb5006
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-20 21:23:39 +0100

    add(models): episode full collection model

---
+++ src/main/java/spotify/models/episodes/EpisodeFullCollection.java
@@ -0,0 +1,15 @@
+package spotify.models.episodes;
+
+import java.util.List;
+
+public class EpisodeFullCollection {
+    private List<EpisodeFull> episodes;
+
+    public List<EpisodeFull> getEpisodes() {
+        return episodes;
+    }
+
+    public void setEpisodes(List<EpisodeFull> episodes) {
+        this.episodes = episodes;
+    }
+}
