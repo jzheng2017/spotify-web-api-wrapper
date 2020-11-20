a97adf4e3054c82138717df06b1db9ec82d725d8
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-20 22:16:50 +0100

    add(retrofit): http call for fetching episodes of a show

--- src/main/java/spotify/retrofit/services/ShowService.java
+++ src/main/java/spotify/retrofit/services/ShowService.java
@@ -5,9 +5,18 @@ import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
+import spotify.models.episodes.EpisodeSimplified;
+import spotify.models.paging.Paging;
 import spotify.models.shows.ShowFull;
 
 public interface ShowService {
     @GET("shows/{id}")
     Call<ShowFull> getShow(@Header("Authorization") String accessToken, @Path("id") String showId, @Query("market") String market);
+
+    @GET("shows/{id}/episodes")
+    Call<Paging<EpisodeSimplified>> getShowEpisodes(@Header("Authorization") String accessToken,
+                                                    @Path("id") String showId,
+                                                    @Query("limit") int limit,
+                                                    @Query("offset") int offset,
+                                                    @Query("market") String market);
 }
