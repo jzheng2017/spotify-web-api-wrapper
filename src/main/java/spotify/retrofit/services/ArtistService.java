e799c0b9d536f3cbf17dbcb3563e51f9fa6cf606
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-20 23:49:25 +0100

    add(retrofit): http call for fetching albums of an artist

--- src/main/java/spotify/retrofit/services/ArtistService.java
+++ src/main/java/spotify/retrofit/services/ArtistService.java
@@ -4,9 +4,20 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 import spotify.models.artists.ArtistFull;
+import spotify.models.artists.ArtistSimplified;
+import spotify.models.paging.Paging;
 
 public interface ArtistService {
     @GET("artists/{id}")
     Call<ArtistFull> getArtist(@Header("Authorization") String accessToken, @Path("id") String artistId);
+
+    @GET("artists/{id}/albums")
+    Call<Paging<ArtistSimplified>> getArtistAlbums(@Header("Authorization") String accessToken,
+                                                   @Path("id") String artistId,
+                                                   @Query("include_groups") String albumTypes,
+                                                   @Query("country") String country,
+                                                   @Query("limit") int limit,
+                                                   @Query("offset") int offset);
 }
