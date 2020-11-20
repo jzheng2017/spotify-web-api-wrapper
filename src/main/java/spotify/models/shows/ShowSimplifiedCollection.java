cab70d9c664b4543642e424c1c3070769e48d64a
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-20 22:28:01 +0100

    add(models): collection of simplified show objects

---
+++ src/main/java/spotify/models/shows/ShowSimplifiedCollection.java
@@ -0,0 +1,15 @@
+package spotify.models.shows;
+
+import java.util.List;
+
+public class ShowSimplifiedCollection {
+    private List<ShowSimplified> shows;
+
+    public List<ShowSimplified> getShows() {
+        return shows;
+    }
+
+    public void setShows(List<ShowSimplified> shows) {
+        this.shows = shows;
+    }
+}
