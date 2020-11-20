9f53f42b0387619f070aa5264347d78345b3140d
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-20 20:53:45 +0100

    fix(retrofit): missing path param annotation

--- src/main/java/spotify/retrofit/services/UserService.java
+++ src/main/java/spotify/retrofit/services/UserService.java
@@ -3,6 +3,7 @@ package spotify.retrofit.services;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Path;
 import spotify.models.users.User;
 
 public interface UserService {
@@ -10,5 +11,5 @@ public interface UserService {
     Call<User> getCurrentUser(@Header("Authorization") String accessToken);
 
     @GET("users/{user_id}")
-    Call<User> getUserById(@Header("Authorization") String accessToken, String userId);
+    Call<User> getUserById(@Header("Authorization") String accessToken, @Path("user_id") String userId);
 }
