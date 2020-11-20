9d34dcdd39f0597a3ca676e6283ff59dd716f598
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-20 21:24:03 +0100

    add(retrofit): http call to fetch multiple episodes

--- src/main/java/spotify/retrofit/services/EpisodeService.java
+++ src/main/java/spotify/retrofit/services/EpisodeService.java
@@ -4,9 +4,14 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 import spotify.models.episodes.EpisodeFull;
+import spotify.models.episodes.EpisodeFullCollection;
 
 public interface EpisodeService {
     @GET("episodes/{id}")
     Call<EpisodeFull> getEpisode(@Header("Authorization") String accessToken, @Path("id") String episodeId);
+
+    @GET("episodes")
+    Call<EpisodeFullCollection> getEpisodes(@Header("Authorization") String accessToken, @Query("ids") String episodeIds, @Query("market") String market);
 }
