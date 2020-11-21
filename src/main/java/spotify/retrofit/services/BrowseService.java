f178eb57a8ea175b8f49aa6fcc2d6594d9dec343
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-21 17:36:57 +0100

    add(retrofit): http call to fetch recommendations

--- src/main/java/spotify/retrofit/services/BrowseService.java
+++ src/main/java/spotify/retrofit/services/BrowseService.java
@@ -5,10 +5,12 @@ import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Path;
 import retrofit2.http.QueryMap;
+import spotify.models.albums.AlbumSimplifiedPaging;
 import spotify.models.categories.CategoryFull;
 import spotify.models.categories.CategoryFullPaging;
 import spotify.models.playlists.FeaturedPlaylistCollection;
 import spotify.models.playlists.PlaylistSimplifiedPaging;
+import spotify.models.recommendations.RecommendationCollection;
 
 import java.util.Map;
 
@@ -24,4 +26,10 @@ public interface BrowseService {
 
     @GET("browse/featured-playlists")
     Call<FeaturedPlaylistCollection> getFeaturedPlaylists(@Header("Authorization") String accessToken, @QueryMap Map<String, String> options);
+
+    @GET("browse/new-releases")
+    Call<AlbumSimplifiedPaging> getNewReleases(@Header("Authorization") String accessToken, @QueryMap Map<String, String> options);
+
+    @GET("recommendations")
+    Call<RecommendationCollection> getRecommendations(@Header("Authorization") String accessToken, @QueryMap Map<String, String> options);
 }
