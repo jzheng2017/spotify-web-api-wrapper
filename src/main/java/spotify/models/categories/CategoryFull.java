32bdb387bd3c1ff959e0e4ce8bd7096b94ea9cd1
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-21 15:02:31 +0100

    add(models): category full model

---
+++ src/main/java/spotify/models/categories/CategoryFull.java
@@ -0,0 +1,44 @@
+package spotify.models.categories;
+
+import spotify.models.generic.Image;
+
+import java.util.List;
+
+public class CategoryFull {
+    private String href;
+    private List<Image> icons;
+    private String id;
+    private String name;
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    public List<Image> getIcons() {
+        return icons;
+    }
+
+    public void setIcons(List<Image> icons) {
+        this.icons = icons;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
