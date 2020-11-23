6f67ddb13e79c971456a8cfa915c0bbede17e259
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-23 21:27:51 +0100

    add(factories): factory for creating retrofit services

---
+++ src/main/java/spotify/factories/RetrofitHttpServiceFactory.java
@@ -0,0 +1,59 @@
+package spotify.factories;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import retrofit2.Retrofit;
+import spotify.config.ApiUrl;
+import spotify.retrofit.services.*;
+
+public class RetrofitHttpServiceFactory {
+    private final static Logger logger = LoggerFactory.getLogger(RetrofitHttpServiceFactory.class);
+
+    private static <T> T getRetrofitHttpService(final Class<T> serviceClassToBeCreatedFor, final String baseUrl) {
+        logger.trace("Requesting Retrofit HTTP client.");
+        Retrofit httpClient = RetrofitClientFactory.getRetrofitClient(baseUrl);
+
+        return httpClient.create(serviceClassToBeCreatedFor);
+    }
+
+    public static AlbumService getAlbumService() {
+        return getRetrofitHttpService(AlbumService.class, ApiUrl.API_URL_HTTPS + ApiUrl.VERSION);
+    }
+
+    public static ArtistService getArtistService() {
+        return getRetrofitHttpService(ArtistService.class, ApiUrl.API_URL_HTTPS + ApiUrl.VERSION);
+    }
+
+    public static AuthorizationCodeFlowService getAuthorizationCodeFlowService() {
+        return getRetrofitHttpService(AuthorizationCodeFlowService.class, ApiUrl.ACCOUNTS_URL_HTTPS);
+    }
+
+    public static BrowseService getBrowseService() {
+        return getRetrofitHttpService(BrowseService.class, ApiUrl.API_URL_HTTPS + ApiUrl.VERSION);
+    }
+
+    public static ClientCredentialsFlowService getClientCredentialsFlowService() {
+        return getRetrofitHttpService(ClientCredentialsFlowService.class, ApiUrl.ACCOUNTS_URL_HTTPS);
+    }
+
+    public static EpisodeService getEpisodeService() {
+        return getRetrofitHttpService(EpisodeService.class, ApiUrl.API_URL_HTTPS + ApiUrl.VERSION);
+    }
+
+    public static ShowService getShowService() {
+        return getRetrofitHttpService(ShowService.class, ApiUrl.API_URL_HTTPS + ApiUrl.VERSION);
+    }
+
+    public static TrackService getTrackService() {
+        return getRetrofitHttpService(TrackService.class, ApiUrl.API_URL_HTTPS + ApiUrl.VERSION);
+    }
+
+    public static UserService getUserService() {
+        return getRetrofitHttpService(UserService.class, ApiUrl.API_URL_HTTPS + ApiUrl.VERSION);
+    }
+
+    public static FollowService getFollowService() {
+        return getRetrofitHttpService(FollowService.class, ApiUrl.API_URL_HTTPS + ApiUrl.VERSION);
+    }
+
+}
