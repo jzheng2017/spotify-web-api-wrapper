b7fccfe4538a4dbf6d1dd25c3640ff4a6bfcfe06
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-23 22:20:29 +0100

    add(retrofit): http call to check if users are following a playlist

--- src/main/java/spotify/retrofit/services/FollowService.java
+++ src/main/java/spotify/retrofit/services/FollowService.java
@@ -3,6 +3,7 @@ package spotify.retrofit.services;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.Path;
 import retrofit2.http.Query;
 import spotify.api.enums.EntityType;
 
@@ -11,4 +12,7 @@ import java.util.List;
 public interface FollowService {
     @GET("me/following/contains")
     Call<List<Boolean>> isFollowing(@Header("Authorization") String accessToken, @Query("type") EntityType entityType, @Query("ids") String entityIds);
+
+    @GET("playlists/{playlist_id}/followers/contains")
+    Call<List<Boolean>> isFollowingPlaylist(@Header("Authorization") String accessToken, @Path("playlist_id") String playlistId, @Query("ids") String userIds);
 }
