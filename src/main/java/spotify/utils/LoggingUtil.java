e97d4e0938d1b026b5b512dcf3283df7bf9787e3
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-23 22:15:31 +0100

    add(utils): logging util for duplicate logging

---
+++ src/main/java/spotify/utils/LoggingUtil.java
@@ -0,0 +1,11 @@
+package spotify.utils;
+
+import org.slf4j.Logger;
+import retrofit2.Call;
+
+public class LoggingUtil {
+
+    public static <T> void logHttpCall(final Logger logger, final Call<T> httpCall) {
+        logger.debug(String.format("%s / %s", httpCall.request().method(), httpCall.request().url().toString()));
+    }
+}
