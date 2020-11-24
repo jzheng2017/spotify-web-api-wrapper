1765ecd23c8d98ce99cb09a69784e494761c6a70
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-24 20:46:02 +0100

    add(retrofit): http call for following playlist

--- src/main/java/spotify/retrofit/services/FollowService.java
+++ src/main/java/spotify/retrofit/services/FollowService.java
@@ -3,6 +3,7 @@ package spotify.retrofit.services;
 import retrofit2.Call;
 import retrofit2.http.*;
 import spotify.api.enums.EntityType;
+import spotify.models.playlists.FollowPlaylistRequestBody;
 
 import java.util.List;
 
@@ -15,4 +16,9 @@ public interface FollowService {
 
     @PUT("me/following")
     Call<Void> followEntity(@Header("Authorization") String accessToken, @Query("type") EntityType entityType, @Query("ids") String entityIds);
+
+    @PUT("playlists/{playlist_id}/followers")
+    Call<Void> followPlaylist(@Header("Authorization") String accessToken,
+                              @Path("playlist_id") String playlistId,
+                              @Body FollowPlaylistRequestBody setPlaylistPublic);
 }
