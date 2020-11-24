03c4e6591d13f7d243e5b86a8f5fd808b25d9fb3
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-24 21:14:16 +0100

    add(retrofit): http call for fetching followed artists

--- src/main/java/spotify/retrofit/services/FollowService.java
+++ src/main/java/spotify/retrofit/services/FollowService.java
@@ -3,9 +3,11 @@ package spotify.retrofit.services;
 import retrofit2.Call;
 import retrofit2.http.*;
 import spotify.api.enums.EntityType;
+import spotify.models.artists.ArtistFullCursorBasedPagingWrapper;
 import spotify.models.playlists.FollowPlaylistRequestBody;
 
 import java.util.List;
+import java.util.Map;
 
 public interface FollowService {
     @GET("me/following/contains")
@@ -16,6 +18,11 @@ public interface FollowService {
 
     @PUT("me/following")
     Call<Void> followEntity(@Header("Authorization") String accessToken, @Query("type") EntityType entityType, @Query("ids") String entityIds);
+
+    @GET("me/following")
+    Call<ArtistFullCursorBasedPagingWrapper> getFollowedArtists(@Header("Authorization") String accessToken,
+                                                                @Query("type") EntityType entityType,
+                                                                @QueryMap Map<String, String> options);
 
     @PUT("playlists/{playlist_id}/followers")
     Call<Void> followPlaylist(@Header("Authorization") String accessToken,
