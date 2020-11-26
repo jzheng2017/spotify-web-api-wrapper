ac5f4ad60f49d9a72c4aabe6bdf5e3961b399610
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-26 22:37:04 +0100

    refactor(factories): extracted same value to constant

--- src/main/java/spotify/factories/RetrofitHttpServiceFactory.java
+++ src/main/java/spotify/factories/RetrofitHttpServiceFactory.java
@@ -7,7 +7,8 @@ import spotify.config.ApiUrl;
 import spotify.retrofit.services.*;
 
 public class RetrofitHttpServiceFactory {
-    private final static Logger logger = LoggerFactory.getLogger(RetrofitHttpServiceFactory.class);
+    private static final String API_BASE_URL_HTTPS_WITH_VERSION = ApiUrl.API_URL_HTTPS + ApiUrl.VERSION;
+    private static final Logger logger = LoggerFactory.getLogger(RetrofitHttpServiceFactory.class);
 
     private static <T> T getRetrofitHttpService(final Class<T> serviceClassToBeCreatedFor, final String baseUrl) {
         logger.trace("Requesting Retrofit HTTP client.");
@@ -17,11 +18,11 @@ public class RetrofitHttpServiceFactory {
     }
 
     public static AlbumService getAlbumService() {
-        return getRetrofitHttpService(AlbumService.class, ApiUrl.API_URL_HTTPS + ApiUrl.VERSION);
+        return getRetrofitHttpService(AlbumService.class, API_BASE_URL_HTTPS_WITH_VERSION);
     }
 
     public static ArtistService getArtistService() {
-        return getRetrofitHttpService(ArtistService.class, ApiUrl.API_URL_HTTPS + ApiUrl.VERSION);
+        return getRetrofitHttpService(ArtistService.class, API_BASE_URL_HTTPS_WITH_VERSION);
     }
 
     public static AuthorizationCodeFlowService getAuthorizationCodeFlowService() {
@@ -29,7 +30,7 @@ public class RetrofitHttpServiceFactory {
     }
 
     public static BrowseService getBrowseService() {
-        return getRetrofitHttpService(BrowseService.class, ApiUrl.API_URL_HTTPS + ApiUrl.VERSION);
+        return getRetrofitHttpService(BrowseService.class, API_BASE_URL_HTTPS_WITH_VERSION);
     }
 
     public static ClientCredentialsFlowService getClientCredentialsFlowService() {
@@ -37,26 +38,26 @@ public class RetrofitHttpServiceFactory {
     }
 
     public static EpisodeService getEpisodeService() {
-        return getRetrofitHttpService(EpisodeService.class, ApiUrl.API_URL_HTTPS + ApiUrl.VERSION);
+        return getRetrofitHttpService(EpisodeService.class, API_BASE_URL_HTTPS_WITH_VERSION);
     }
 
     public static ShowService getShowService() {
-        return getRetrofitHttpService(ShowService.class, ApiUrl.API_URL_HTTPS + ApiUrl.VERSION);
+        return getRetrofitHttpService(ShowService.class, API_BASE_URL_HTTPS_WITH_VERSION);
     }
 
     public static TrackService getTrackService() {
-        return getRetrofitHttpService(TrackService.class, ApiUrl.API_URL_HTTPS + ApiUrl.VERSION);
+        return getRetrofitHttpService(TrackService.class, API_BASE_URL_HTTPS_WITH_VERSION);
     }
 
     public static UserService getUserService() {
-        return getRetrofitHttpService(UserService.class, ApiUrl.API_URL_HTTPS + ApiUrl.VERSION);
+        return getRetrofitHttpService(UserService.class, API_BASE_URL_HTTPS_WITH_VERSION);
     }
 
     public static FollowService getFollowService() {
-        return getRetrofitHttpService(FollowService.class, ApiUrl.API_URL_HTTPS + ApiUrl.VERSION);
+        return getRetrofitHttpService(FollowService.class, API_BASE_URL_HTTPS_WITH_VERSION);
     }
 
     public static LibraryService getLibraryService() {
-        return getRetrofitHttpService(LibraryService.class, ApiUrl.API_URL_HTTPS + ApiUrl.VERSION);
+        return getRetrofitHttpService(LibraryService.class, API_BASE_URL_HTTPS_WITH_VERSION);
     }
 }
