a71ff20bc2c7edb0e1045e32551c11fadd84ea94
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-28 22:13:59 +0100

    add(models): base class for playable objects

---
+++ src/main/java/spotify/models/generic/AbstractPlayableObject.java
@@ -0,0 +1,85 @@
+package spotify.models.generic;
+
+public class AbstractPlayableObject {
+    protected int durationMs;
+    protected boolean explicit;
+    protected ExternalUrl externalUrls;
+    protected String href;
+    protected String id;
+    protected boolean isPlayable;
+    protected String name;
+    protected String type;
+    protected String uri;
+
+    public int getDurationMs() {
+        return durationMs;
+    }
+
+    public void setDurationMs(int durationMs) {
+        this.durationMs = durationMs;
+    }
+
+    public boolean isExplicit() {
+        return explicit;
+    }
+
+    public void setExplicit(boolean explicit) {
+        this.explicit = explicit;
+    }
+
+    public ExternalUrl getExternalUrls() {
+        return externalUrls;
+    }
+
+    public void setExternalUrls(ExternalUrl externalUrls) {
+        this.externalUrls = externalUrls;
+    }
+
+    public String getHref() {
+        return href;
+    }
+
+    public void setHref(String href) {
+        this.href = href;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public boolean isPlayable() {
+        return isPlayable;
+    }
+
+    public void setPlayable(boolean playable) {
+        isPlayable = playable;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getUri() {
+        return uri;
+    }
+
+    public void setUri(String uri) {
+        this.uri = uri;
+    }
+}
