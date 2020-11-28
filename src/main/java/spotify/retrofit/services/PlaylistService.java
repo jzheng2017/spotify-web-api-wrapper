447e022fa9b824f0a4343a77d800d15eb32102a2
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-28 17:26:50 +0100

    add(retrofit): http call to fetch a playlist

--- src/main/java/spotify/retrofit/services/PlaylistService.java
+++ src/main/java/spotify/retrofit/services/PlaylistService.java
@@ -5,9 +5,12 @@ import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.Path;
 import retrofit2.http.QueryMap;
+import spotify.models.generic.Image;
 import spotify.models.paging.Paging;
+import spotify.models.playlists.PlaylistFull;
 import spotify.models.playlists.PlaylistSimplified;
 
+import java.util.List;
 import java.util.Map;
 
 public interface PlaylistService {
@@ -16,4 +19,10 @@ public interface PlaylistService {
 
     @GET("users/{user_id}/playlists")
     Call<Paging<PlaylistSimplified>> getUserPlaylists(@Header("Authorization") String accessToken, @Path("user_id") String userId, @QueryMap Map<String, String> options);
+
+    @GET("playlists/{playlist_id}/images")
+    Call<List<Image>> getPlaylistCoverImages(@Header("Authorization") String accessToken, @Path("playlist_id") String playlistId);
+
+    @GET("playlists/{playlist_id}")
+    Call<PlaylistFull> getPlaylist(@Header("Authorization") String accessToken, @Path("playlist_id") String playlistId, @QueryMap Map<String, String> options);
 }
