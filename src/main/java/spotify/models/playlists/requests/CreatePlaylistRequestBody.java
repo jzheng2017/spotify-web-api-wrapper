6d50a963cddeea21fa7b4dd70c5e11497c1a75a1
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-11-29 15:42:15 +0100

    add(models): request body model for creating a playlist

---
+++ src/main/java/spotify/models/playlists/requests/CreatePlaylistRequestBody.java
@@ -0,0 +1,50 @@
+package spotify.models.playlists.requests;
+
+import com.google.gson.annotations.SerializedName;
+
+public class CreatePlaylistRequestBody {
+    private String name;
+    private String description;
+    @SerializedName("public")
+    private boolean isPublic;
+    private boolean collaborative;
+
+    public CreatePlaylistRequestBody(String name, String description, boolean isPublic, boolean collaborative) {
+        this.name = name;
+        this.description = description;
+        this.isPublic = isPublic;
+        this.collaborative = collaborative;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isPublic() {
+        return isPublic;
+    }
+
+    public void setPublic(boolean aPublic) {
+        isPublic = aPublic;
+    }
+
+    public boolean isCollaborative() {
+        return collaborative;
+    }
+
+    public void setCollaborative(boolean collaborative) {
+        this.collaborative = collaborative;
+    }
+}
