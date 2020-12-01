26b51ed7b3b43f5702bad63461601926f3af95e4
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-12-01 17:32:49 +0100

    add(models): collection of devices

---
+++ src/main/java/spotify/models/players/Device.java
@@ -0,0 +1,67 @@
+package spotify.models.players;
+
+public class Device {
+    private String id;
+    private String name;
+    private String type;
+    private boolean isActive;
+    private boolean isPrivateSession;
+    private boolean isRestricted;
+    private int volumePercent;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public boolean isActive() {
+        return isActive;
+    }
+
+    public void setActive(boolean active) {
+        isActive = active;
+    }
+
+    public boolean isPrivateSession() {
+        return isPrivateSession;
+    }
+
+    public void setPrivateSession(boolean privateSession) {
+        isPrivateSession = privateSession;
+    }
+
+    public boolean isRestricted() {
+        return isRestricted;
+    }
+
+    public void setRestricted(boolean restricted) {
+        isRestricted = restricted;
+    }
+
+    public int getVolumePercent() {
+        return volumePercent;
+    }
+
+    public void setVolumePercent(int volumePercent) {
+        this.volumePercent = volumePercent;
+    }
+}
---
+++ src/main/java/spotify/models/players/DeviceCollection.java
@@ -0,0 +1,15 @@
+package spotify.models.players;
+
+import java.util.List;
+
+public class DeviceCollection {
+    private List<Device> devices;
+
+    public List<Device> getDevices() {
+        return devices;
+    }
+
+    public void setDevices(List<Device> devices) {
+        this.devices = devices;
+    }
+}
