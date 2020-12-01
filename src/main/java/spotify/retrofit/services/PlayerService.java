a6a0aafeb58b460c4e5a2661639fc7996457e3ed
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-12-01 18:59:14 +0100

    add(retrofit): http call to fetch current playing context

--- src/main/java/spotify/retrofit/services/PlayerService.java
+++ src/main/java/spotify/retrofit/services/PlayerService.java
@@ -3,9 +3,16 @@ package spotify.retrofit.services;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.QueryMap;
 import spotify.models.players.DeviceCollection;
+import spotify.models.players.PlayingContext;
+
+import java.util.Map;
 
 public interface PlayerService {
     @GET("me/player/devices")
     Call<DeviceCollection> getAvailableDevices(@Header("Authorization") String accessToken);
+
+    @GET("me/player")
+    Call<PlayingContext> getCurrentPlayingContext(@Header("Authorization") String accessToken, @QueryMap Map<String, String> options);
 }
