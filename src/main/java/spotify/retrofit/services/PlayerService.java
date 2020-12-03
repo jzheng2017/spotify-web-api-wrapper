6d35fd6e767432ec02db191aebb2a6f39a42646e
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-12-03 19:06:36 +0100

    add(retrofit): http call to add item to the queue

--- src/main/java/spotify/retrofit/services/PlayerService.java
+++ src/main/java/spotify/retrofit/services/PlayerService.java
@@ -1,9 +1,7 @@
 package spotify.retrofit.services;
 
 import retrofit2.Call;
-import retrofit2.http.GET;
-import retrofit2.http.Header;
-import retrofit2.http.QueryMap;
+import retrofit2.http.*;
 import spotify.models.paging.CursorBasedPaging;
 import spotify.models.players.CurrentlyPlayingObject;
 import spotify.models.players.DeviceCollection;
@@ -24,4 +22,7 @@ public interface PlayerService {
 
     @GET("me/player/currently-playing")
     Call<CurrentlyPlayingObject> getCurrentlyPlayingObject(@Header("Authorization") String accessToken, @QueryMap Map<String, String> options);
+
+    @POST("me/player/queue")
+    Call<Void> addItemToQueue(@Header("Authorization") String accessToken, @Query("uri") String uri, @QueryMap Map<String, String> options);
 }
