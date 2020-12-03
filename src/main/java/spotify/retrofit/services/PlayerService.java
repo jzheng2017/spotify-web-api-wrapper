04145d6580594c0ef44b4617d6f73813be7baa94
Jiankai Zheng <J.Zheng1@student.han.nl>
2020-12-03 18:39:52 +0100

    add(retrofit): http call to fetch recently played tracks

--- src/main/java/spotify/retrofit/services/PlayerService.java
+++ src/main/java/spotify/retrofit/services/PlayerService.java
@@ -4,7 +4,9 @@ import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.QueryMap;
+import spotify.models.paging.CursorBasedPaging;
 import spotify.models.players.DeviceCollection;
+import spotify.models.players.PlayHistory;
 import spotify.models.players.PlayingContext;
 
 import java.util.Map;
@@ -15,4 +17,7 @@ public interface PlayerService {
 
     @GET("me/player")
     Call<PlayingContext> getCurrentPlayingContext(@Header("Authorization") String accessToken, @QueryMap Map<String, String> options);
+
+    @GET("me/player/recently-played")
+    Call<CursorBasedPaging<PlayHistory>> getRecentlyPlayedTracks(@Header("Authorization") String accessToken, @QueryMap Map<String, String> options);
 }
