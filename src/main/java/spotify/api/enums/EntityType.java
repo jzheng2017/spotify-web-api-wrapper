6d1cc4a7c811bdd4279233e36a378997275991af
Krista Nittmann <cieloangel@cox.net>
2021-01-08 17:59:32 -0600

    docs: corrected JavaDoc comments for the EntityType enum class [skip ci]

--- src/main/java/spotify/api/enums/EntityType.java
+++ src/main/java/spotify/api/enums/EntityType.java
@@ -2,8 +2,8 @@ package spotify.api.enums;
 
 
 /**
- * This class contains and sets the suitable values for the entity type. The entity
- * type is used when a user follows either another user or an artist.
+ * This class contains all suitable entity types. The entity type is used to determine what type of
+ * entity a user chooses to follow.
  * <p>
  * For further information see: <a href="https://developer.spotify.com/documentation/web-api/reference/follow/">
  * Follow</a>.
@@ -19,9 +19,9 @@ public enum EntityType {
     private final String value;
 
     /**
-     * Set the entity type.
+     * Constructs an enum value accepting a string argument whose value is the string representation of itself.
      * 
-     * @param value The type of entity the user is following.
+     * @param value The entity type to be constructed.
      */
     EntityType(String value) {
         this.value = value;
