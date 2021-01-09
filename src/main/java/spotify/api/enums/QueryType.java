38a73aaeed626610fc777332b95494cc407e5d74
Krista Nittmann <cieloangel@cox.net>
2021-01-08 18:10:27 -0600

    docs: corrected JavaDoc comments for the QueryType enum class [skip ci]

--- src/main/java/spotify/api/enums/QueryType.java
+++ src/main/java/spotify/api/enums/QueryType.java
@@ -1,9 +1,10 @@
 package spotify.api.enums;
 
 /**
- * This class contains and sets the query type used when searching Spotify.
+ * This class contains suitable query types used when searching Spotify.
  * <p>
- * For further information see: <a href="https://developer.spotify.com/documentation/web-api/reference/search/search/">Search</a>.
+ * For further information see: <a href="https://developer.spotify.com/documentation/web-api/reference/search/search/">
+ * Search</a>.
  *
  * @author Jiankai Zheng (jk.zheng@hotmail.com)
  * @since 1.0.0
@@ -20,9 +21,9 @@ public enum QueryType {
     private final String value;
 
      /**
-     * Set the query type.
+     * Constructs an enum value accepting a string argument whose value is the string representation of itself.
      * 
-     * @param value The query type.
+     * @param value The query type to be constructed.
      */
     QueryType(String value) {
         this.value = value;
